e4708162da9924b8817f760bb109449a3b15e4ab
Jesse Croxford <jcroxford@simplenexus.com>
2019-12-05 17:48:59 -0700

    added solution for day 5 part 2

--- src/days/5.ts
+++ src/days/5.ts
@@ -4,18 +4,27 @@ import { IntCodeCompiler } from './../shared/intCodeCompiler'
 // ==============================================================================================================
 // exports
 // ==============================================================================================================
+// todo allow input to be programmatic
+// todo move all conditional checks to functions
+// todo print memeory address with print output?
 export function main () {
   const code = getAocDataForDay(5)
 
-  console.log('---------- running part 1 ----------\n')
-  console.log(part1(code))
-  console.log('---------- part 1 finished ---------\n')
+  // console.log('---------- running part 1 ----------\n')
+  // console.log(part1(code))
+  // console.log('---------- part 1 finished ---------\n')
+
+  console.log('---------- part 2 finished ---------\n')
+  console.log(part2(code))
+  console.log('---------- part 2 finished ---------\n')
 }
 
 export function part1 (code: string) {
   const program = new IntCodeCompiler(code)
   program.execute()
 }
+
+export function part2 (code: string) { return part1(code) }
 
 // ==============================================================================================================
 // helper methods
--- src/shared/intCodeCompiler.ts
+++ src/shared/intCodeCompiler.ts
@@ -6,6 +6,10 @@ export class IntCodeCompiler {
   static MULT_INSTRUCTION = 2
   static SAVE_INSTRUCTION = 3
   static PRINT_INSTRUCTION = 4
+  static JUMP_IF_TRUE_INSTRUCTION = 5
+  static JUMP_IF_FALSE_INSTRUCTION = 6
+  static IS_LESS_THAN_INSTRUCTION = 7
+  static IS_EQUAL_INSTRUCTION = 8
   static HALT_INSTRUCTION = 99
 
   public memory: number[]
@@ -18,7 +22,6 @@ export class IntCodeCompiler {
    * right now assumes whatever is at index 0 after execution should be the return value
    */
   execute () {
-
     let indexOfCurrentOpcode = 0
     let currentIntsruction = null
     while (currentIntsruction !== IntCodeCompiler.HALT_INSTRUCTION && indexOfCurrentOpcode < this.memory.length) {
@@ -59,6 +62,42 @@ export class IntCodeCompiler {
         console.log('printing arg', argOne)
 
         indexOfCurrentOpcode += 2
+      }
+
+      if (currentIntsruction === IntCodeCompiler.JUMP_IF_TRUE_INSTRUCTION) {
+        const argOne = this.useImmediateMode(modes[0]) ? this.memory[indexOfCurrentOpcode + 1] : this.memory[this.memory[indexOfCurrentOpcode + 1]]
+        const argTwo = this.useImmediateMode(modes[1]) ? this.memory[indexOfCurrentOpcode + 2] : this.memory[this.memory[indexOfCurrentOpcode + 2]]
+
+        if (argOne) indexOfCurrentOpcode = argTwo
+        else indexOfCurrentOpcode += 3
+      }
+
+      if (currentIntsruction === IntCodeCompiler.JUMP_IF_FALSE_INSTRUCTION) {
+        const argOne = this.useImmediateMode(modes[0]) ? this.memory[indexOfCurrentOpcode + 1] : this.memory[this.memory[indexOfCurrentOpcode + 1]]
+        const argTwo = this.useImmediateMode(modes[1]) ? this.memory[indexOfCurrentOpcode + 2] : this.memory[this.memory[indexOfCurrentOpcode + 2]]
+
+        if (argOne === 0) indexOfCurrentOpcode = argTwo
+        else indexOfCurrentOpcode += 3
+      }
+
+      if (currentIntsruction === IntCodeCompiler.IS_LESS_THAN_INSTRUCTION) {
+        const argOne = this.useImmediateMode(modes[0]) ? this.memory[indexOfCurrentOpcode + 1] : this.memory[this.memory[indexOfCurrentOpcode + 1]]
+        const argTwo = this.useImmediateMode(modes[1]) ? this.memory[indexOfCurrentOpcode + 2] : this.memory[this.memory[indexOfCurrentOpcode + 2]]
+
+        // writes never immediate
+        this.memory[this.memory[indexOfCurrentOpcode + 3]] = argOne < argTwo ? 1 : 0
+
+        indexOfCurrentOpcode += 4
+      }
+
+      if (currentIntsruction === IntCodeCompiler.IS_EQUAL_INSTRUCTION) {
+        const argOne = this.useImmediateMode(modes[0]) ? this.memory[indexOfCurrentOpcode + 1] : this.memory[this.memory[indexOfCurrentOpcode + 1]]
+        const argTwo = this.useImmediateMode(modes[1]) ? this.memory[indexOfCurrentOpcode + 2] : this.memory[this.memory[indexOfCurrentOpcode + 2]]
+
+        // writes never immediate
+        this.memory[this.memory[indexOfCurrentOpcode + 3]] = argOne === argTwo ? 1 : 0
+
+        indexOfCurrentOpcode += 4
       }
     }
 
