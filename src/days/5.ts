1f9b44cc39f92b52e726974dd647c4ffb6a69ac8
Jesse Croxford <jcroxford@simplenexus.com>
2019-12-07 18:05:07 -0700

    fixed day 5. now works with refactored compiler code

--- src/days/5.ts
+++ src/days/5.ts
@@ -10,21 +10,21 @@ import { IntCodeCompiler } from './../shared/intCodeCompiler'
 export function main () {
   const code = getAocDataForDay(5)
 
-  // console.log('---------- running part 1 ----------\n')
-  // console.log(part1(code))
-  // console.log('---------- part 1 finished ---------\n')
+  console.log('---------- running part 1 ----------\n')
+  console.log(part1(code))
+  console.log('---------- part 1 finished ---------\n')
 
   console.log('---------- part 2 finished ---------\n')
   console.log(part2(code))
   console.log('---------- part 2 finished ---------\n')
 }
 
-export function part1 (code: string) {
-  const program = new IntCodeCompiler(code)
-  program.execute()
+export function part1 (code: string, input: number[] = [1]) {
+  const program = new IntCodeCompiler(code, input)
+  return program.execute().programOutput
 }
 
-export function part2 (code: string) { return part1(code) }
+export function part2 (code: string) { return part1(code, [5]) }
 
 // ==============================================================================================================
 // helper methods
