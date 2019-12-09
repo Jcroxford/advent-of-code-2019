376fe1b571eb54d2696e6d97e8c2d128aa72d9cb
Jesse Croxford <jcroxford@simplenexus.com>
2019-12-08 22:11:51 -0700

    removed junkyard code

--- src/days/7.ts
+++ src/days/7.ts
@@ -106,33 +106,3 @@ function factorialPermutations (list: number[]): number[][] {
 
   return permutations
 }
-
-// function printArr (a: number[], n: number) {
-//   for (let i = 0; i < n; i++) {
-//     console.log(a[i])
-//   }
-// }
-
-// function heapPermutation (a: number[], size: number, n: number) {
-//     // if size becomes 1 then prints the obtained
-//     // permutation
-//   if (size === 1) return printArr(a, n)
-
-//   for (let i = 0; i < size; i++) {
-//     heapPermutation(a,size - 1,n)
-//     // if size is odd, swap first and last
-//     // element
-//     if (size % 2 === 1) {
-//       let temp = a[0]
-//       a[0] = a[size - 1]
-//       a[size - 1] = temp
-
-//     // If size is even, swap ith and last
-//     // element
-//     } else {
-//       let temp = a[i]
-//       a[i] = a[size - 1]
-//       a[size - 1] = temp
-//     }
-//   }
-// }
